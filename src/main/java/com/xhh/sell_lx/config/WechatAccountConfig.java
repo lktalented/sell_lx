c67f7691e2832eabb91ff8e4034241326da5cc14
lktalented <715466676@qq.com>
2018-11-05 17:43:41 +0800

    IDEA整合github测试

--- src/main/java/com/xhh/sell_lx/config/WechatAccountConfig.java
+++ src/main/java/com/xhh/sell_lx/config/WechatAccountConfig.java
@@ -24,4 +24,32 @@ public class WechatAccountConfig {
       */
       private String mpAppSecret;
 
+      /**
+       * 商户号
+       */
+       private String mchId;
+       
+       /**
+        * 商户秘钥
+        */
+        private String mchKey;
+        
+        /**
+         * 商户证书路径
+         */
+         private String keyPath;
+         
+         /**
+          * 微信支付异步通知路径
+          */
+          private String notifyUrl;
+
+          
+          
+         
+        
+        
+       
+       
+
 }
---
+++ src/main/java/com/xhh/sell_lx/config/WechatPayConfig.java
@@ -0,0 +1,37 @@
+package com.xhh.sell_lx.config;
+
+import com.lly835.bestpay.config.WxPayH5Config;
+import com.lly835.bestpay.service.impl.BestPayServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * 微信支付配置
+ * luokai
+ * 2018/11/5 0005 下午 5:28
+ */
+@Component
+public class WechatPayConfig {
+
+    @Autowired
+    private WechatAccountConfig wechatAccountConfig;
+
+    @Bean
+    public BestPayServiceImpl bestPayService(){
+        BestPayServiceImpl bestPayService = new BestPayServiceImpl();
+        bestPayService.setWxPayH5Config(wxPayH5Config());
+        return bestPayService;
+    }
+
+    @Bean
+    public WxPayH5Config wxPayH5Config(){
+        WxPayH5Config wxPayH5Config = new WxPayH5Config();
+        wxPayH5Config.setAppId(wechatAccountConfig.getMpAppId());
+        wxPayH5Config.setAppSecret(wechatAccountConfig.getMpAppSecret());
+        wxPayH5Config.setMchId(wechatAccountConfig.getMchId());
+        wxPayH5Config.setMchKey(wechatAccountConfig.getMchKey());
+        wxPayH5Config.setNotifyUrl(wechatAccountConfig.getNotifyUrl());
+        return wxPayH5Config;
+    }
+}
